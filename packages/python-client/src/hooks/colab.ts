737e0cdc1d6dc742989fd0adc63a9528c5fd8ce0
cfeenstra67 <cameron.l.feenstra@gmail.com>
2023-09-10 23:42:22 -0700

    last colab fix?

--- packages/python-client/src/hooks/colab.ts
+++ packages/python-client/src/hooks/colab.ts
@@ -2,8 +2,8 @@ import { useWidgetModelEvent } from "../lib/widget-model";
 
 export function useColabSupport(): void {
   useWidgetModelEvent("msg:custom", async (_, event) => {
-    console.log("Handling custom event", event);
-    if (event?.type === "collab-update" && typeof google !== "undefined") {
+    console.log("Handling custom event", event, typeof google);
+    if (event?.type === "colab-update" && typeof google !== "undefined") {
       console.log("Invoking sync function");
       await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
     }
