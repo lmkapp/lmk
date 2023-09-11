bca23eed3388a2eeb3793bf66d341cf5e232b930
cfeenstra67 <cameron.l.feenstra@gmail.com>
2023-09-11 01:18:13 -0700

    fix

--- packages/python-client/src/hooks/colab.ts
+++ packages/python-client/src/hooks/colab.ts
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
 import { useWidgetModelEvent } from "../lib/widget-model";
 
 export function useColabSupport(): void {
+  const [dirty, setDirty] = useState(0);
+
   useWidgetModelEvent("msg:custom", async (_, event) => {
     // console.log("Handling custom event", event, typeof google);
     if (event?.type === "colab-update" && typeof google !== "undefined") {
-      // console.log(Date.now(), "Invoking sync function");
-      await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      // console.log(Date.now(), "Re-invoking sync function");
-      await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
+      setDirty((val) => val + 1);
     }
   });
+
+  useEffect(() => {
+    if (typeof google === 'undefined') {
+      return;
+    }
+
+    const ivl = setInterval(async () => {
+      console.log('dirty', dirty);
+      if (dirty > 0) {
+        await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
+        setDirty((val) => val - 1);
+      } 
+    }, 500);
+
+    return () => { clearInterval(ivl) };
+  }, []);
 }
