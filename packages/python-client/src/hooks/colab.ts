eb66c94e422e472e8226658d919b46084a64b876
cfeenstra67 <cameron.l.feenstra@gmail.com>
2023-09-11 22:21:52 -0700

    sigh... move back to polling

--- packages/python-client/src/hooks/colab.ts
+++ packages/python-client/src/hooks/colab.ts
@@ -1,28 +1,23 @@
-import { useEffect, useState } from "react";
-import { useWidgetModelEvent } from "../lib/widget-model";
+import { useEffect } from "react";
 
 export function useColabSupport(): void {
-  const [dirty, setDirty] = useState(0);
-
-  useWidgetModelEvent("msg:custom", async (_, event) => {
-    // console.log("Handling custom event", event, typeof google);
-    if (event?.type === "colab-update" && typeof google !== "undefined") {
-      setDirty((val) => val + 1);
-    }
-  });
+  // does not work :(
+  // useWidgetModelEvent("msg:custom", async (_, event) => {
+  //   // console.log("Handling custom event", event, typeof google);
+  //   if (event?.type === "colab-update" && typeof google !== "undefined") {
+  //     await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
+  //     // setDirty((val) => val + 1);
+  //   }
+  // });
 
   useEffect(() => {
     if (typeof google === 'undefined') {
       return;
     }
 
-    const ivl = setInterval(async () => {
-      console.log('dirty', dirty);
-      if (dirty > 0) {
-        await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
-        setDirty((val) => val - 1);
-      } 
-    }, 500);
+    const ivl = setInterval(() => {
+      google.colab.kernel.invokeFunction("lmk.widget.sync", []);
+    }, 2000);
 
     return () => { clearInterval(ivl) };
   }, []);
