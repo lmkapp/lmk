e7e025ee989cbfd8f6e37b24cd14fd2e40587a09
cfeenstra67 <cameron.l.feenstra@gmail.com>
2023-09-11 22:42:20 -0700

    try event-based oneeee more time

--- packages/python-client/src/hooks/colab.ts
+++ packages/python-client/src/hooks/colab.ts
@@ -1,24 +1,26 @@
-import { useEffect } from "react";
+import { useWidgetModelEvent } from "../lib/widget-model";
 
 export function useColabSupport(): void {
-  // does not work :(
-  // useWidgetModelEvent("msg:custom", async (_, event) => {
-  //   // console.log("Handling custom event", event, typeof google);
-  //   if (event?.type === "colab-update" && typeof google !== "undefined") {
-  //     await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
-  //     // setDirty((val) => val + 1);
-  //   }
-  // });
-
-  useEffect(() => {
-    if (typeof google === 'undefined') {
-      return;
+  useWidgetModelEvent("msg:custom", async (_, event) => {
+    // console.log("Handling custom event", event, typeof google);
+    if (event?.type === "colab-update" && typeof google !== "undefined") {
+      // await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      console.log('Invoking sync function');
+      await google.colab.kernel.invokeFunction("lmk.widget.sync", []);
+      // setDirty((val) => val + 1);
     }
+  });
 
-    const ivl = setInterval(() => {
-      google.colab.kernel.invokeFunction("lmk.widget.sync", []);
-    }, 2000);
+  // useEffect(() => {
+  //   if (typeof google === 'undefined') {
+  //     return;
+  //   }
 
-    return () => { clearInterval(ivl) };
-  }, []);
+  //   const ivl = setInterval(() => {
+  //     google.colab.kernel.invokeFunction("lmk.widget.sync", []);
+  //   }, 2000);
+
+  //   return () => { clearInterval(ivl) };
+  // }, []);
 }
