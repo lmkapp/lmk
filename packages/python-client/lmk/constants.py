bf8d7bb3b24764393292e8fb53b77b1614e6008d
cfeenstra67 <cameron.l.feenstra@gmail.com>
2023-09-19 00:59:35 -0700

    Bump version: 1.0.3.dev28 → 1.0.3

--- packages/python-client/lmk/constants.py
+++ packages/python-client/lmk/constants.py
@@ -1,4 +1,4 @@
-VERSION = "1.0.3.dev28"
+VERSION = "1.0.3"
 
 API_URL = "https://api.lmkapp.dev"
 
--- packages/python-client/lmk/jupyter/constants.py
+++ packages/python-client/lmk/jupyter/constants.py
@@ -1,3 +1,3 @@
 MODULE_NAME = "@lmkapp/jupyter-widget"
 
-MODULE_VERSION = "1.0.3-dev28"
+MODULE_VERSION = "1.0.3"
