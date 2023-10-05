e297e411f07200a86dfae3120df85344940e5a68
cfeenstra67 <cameron.l.feenstra@gmail.com>
2023-10-05 01:22:36 -0700

    fix

--- packages/python-client/lmk/process/logging.py
+++ packages/python-client/lmk/process/logging.py
@@ -3,11 +3,21 @@ import logging
 from lmk.process import exc
 
 
+MAPPING = {
+    "CRITICAL": 50,
+    "FATAL": 50,
+    "ERROR": 40,
+    "WARN": 30,
+    "WARNING": 30,
+    "INFO": 20,
+    "DEBUG": 10,
+    "NOTSET": 0,
+}
+
+
 def get_log_level(log_level: str) -> int:
     """ """
-    mapping = logging.getLevelNamesMapping()
-
-    values = set(mapping.values())
+    values = set(MAPPING.values())
 
     if log_level.isdigit():
         log_level_int = int(log_level)
@@ -16,7 +26,7 @@ def get_log_level(log_level: str) -> int:
         return log_level_int
 
     out_log_level = log_level.strip().upper()
-    if out_log_level not in mapping:
+    if out_log_level not in MAPPING:
         raise exc.InvalidLogLevel(log_level)
 
-    return mapping[out_log_level]
+    return MAPPING[out_log_level]
